b2b4493402d5f50ae58c2046bee27b6434fb0198
Bruno Melo <demelobr10@gmail.com>
2023-08-08 09:18:53 -0300

    Ajustes no model Usuario.

--- loocalize/src/main/java/models/Usuario.java
+++ loocalize/src/main/java/models/Usuario.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public abstract class Usuario {
 
-	//Atributos
+		//Atributos
 		private String id;
 		private String usuario;
 		private String senha;
@@ -13,7 +13,6 @@ public abstract class Usuario {
 		
 		//Construtor
 		public Usuario(String usuario, String senha, String email, boolean adm) {
-			this.id = this.gerarId();
 			this.usuario = usuario;
 			this.senha = senha;
 			this.email = email;
@@ -21,6 +20,9 @@ public abstract class Usuario {
 		}
 		
 		//Métodos getts e setts
+		public void setId(String id) {
+			this.id = id;
+		}
 		public String getId() {
 			return id;
 		}
@@ -56,24 +58,11 @@ public abstract class Usuario {
 		public void setAdm(boolean adm) {
 			this.adm = adm;
 		}
-
-		private String gerarId() {
-			String caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-	        StringBuilder sb = new StringBuilder();
-
-	        Random random = new Random();
-	        for (int i = 0; i < 10; i++) {
-	            int index = random.nextInt(caracteres.length());
-	            char caractere = caracteres.charAt(index);
-	            sb.append(caractere);
-	        }
-
-	        return sb.toString();
-		}
 		
 		//toStirng
 		@Override
-		public String toString() {
-			return "Usuario "+ usuario +" possui: \n Id= " + id + " e email " + email + "\n É administrador? " + adm;
+		public String toString(){
+			return String.format("<Administrador>\nNome: %s\nADM: %s", this.getUsuario(), this.isAdm());
 		}
+
 }
