dd9e50e93a0e9e20340461dc8debf7ccaac8ad42
Bruno Melo <demelobr10@gmail.com>
2023-08-08 23:42:58 -0300

    Adicionando as interfaces: IRepositorioVeiculos e IRepositorioLocacoes.

---
+++ loocalize/src/main/java/data/IRepositorioLocacoes.java
@@ -0,0 +1,27 @@
+package data;
+
+import models.Cliente;
+import models.Locacao;
+import models.Promocao;
+import models.Veiculo;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface IRepositorioLocacoes {
+
+    void inserir(Locacao locacao);
+
+    void atualizar(Locacao locacao, Veiculo veiculo, Cliente cliente, Promocao promocao, LocalDateTime dataHoraDaLocacao, int qtdDeDiarias, double valorDaLocacao, boolean carroDevolvido);
+
+    void deletar(Locacao locacao);
+
+    List<Locacao> listarTodasLocacoes();
+
+    boolean existeLocacao(String id);
+
+    Locacao buscarUsuario(String id);
+
+    String gerarId();
+
+}
---
+++ loocalize/src/main/java/data/IRepositorioVeiculos.java
@@ -0,0 +1,24 @@
+package data;
+
+import models.Veiculo;
+
+import java.time.Year;
+import java.util.List;
+
+public interface IRepositorioVeiculos {
+
+    void inserir(Veiculo veiculo);
+
+    void atualizar(Veiculo veiculo, String modelo, String marca, String placa, Year ano, int quilometragem, int qtdDeLocacoes, double valorPorKmRodado, boolean disponivel);
+
+    void deletar(Veiculo veiculo);
+
+    List<Veiculo> listarTodosVeiculos();
+
+    boolean existeVeiculo(String id);
+
+    Veiculo buscarVeiculo(String id);
+
+    String gerarId();
+
+}
