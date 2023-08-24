921af4bcec2e4d2d6f95305b131fb17cc9a1a263
Jose <alexandrelourenco951@gmail.com>
2023-08-24 12:17:39 -0300

    adicionados telas de clientes,veiculos,promoções,colaboradores e colaboradores ADM

---
+++ loocalize/src/main/java/gui/ControladorColaboradores.java
@@ -0,0 +1,21 @@
+package gui;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class ControladorColaboradores {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
+
+
