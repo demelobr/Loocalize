7acb609a9ed96a8db45808681b4627519c65d50c
Bruno Melo <demelobr10@gmail.com>
2023-08-26 12:47:41 -0300

    Ajuste no IControladorPromocoes.

--- loocalize/src/main/java/business/IControladorPromocoes.java
+++ loocalize/src/main/java/business/IControladorPromocoes.java
@@ -1,14 +1,16 @@
 package business;
+import exception.*;
 import models.Promocao;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface IControladorPromocoes {
 
-    void inserirPromocao (Promocao promocao);
+    void inserirPromocao (Promocao promocao) throws PromocaoExisteException, PromocaoNulaException, PromocaoInseridaComSucessoException;
 
-    void atualizarPromocao(Promocao promocao, String titulo, int porcentagemDeDesconto, int qtdMinimaDeDiarias, int qtdMinimaDeLocacoes, LocalDateTime dataDeExpiracao, boolean ativa);
+    void atualizarPromocao(Promocao promocao, String titulo, int porcentagemDeDesconto, int qtdMinimaDeDiarias, int qtdMinimaDeLocacoes, LocalDate dataDeExpiracao, boolean ativa) throws PromocaoNulaException, PromocaoNaoExisteException, PromocaoEditadaComSucessoException;
 
     public void deletarPromocao(Promocao promocao);
 
